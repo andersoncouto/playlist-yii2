3fc23b4f7d9d3cbc46d48d9d339c63c6c3ce1bfb
Anderson Couto <andercouto@gmail.com>
2019-08-15 22:24:51 -0300

    Add ajax function to create the playlist

--- web/js/custom.js
+++ web/js/custom.js
@@ -1,5 +1,19 @@
-$(document).ready(function(){
-    $('div').click(function(){
-        // $(this).hide()
-    });
-}); 
+
+$(document).on('click', "#getPlaylist", function(){
+    $.ajax({
+        url: '/index.php?r=site/get-random-playlist',
+        type: 'post',
+        dataType: 'json',
+        success: function(response) {
+            if(response){
+                $('#modal_playlist').modal('hide')
+                $('#playlistTable').html('<table id="playlistTable" class="table table-striped table-sm table-dark">')
+                $('#playlistTable').html('<thead><tr><th>#</th><th>Música</th><th>Duração (min)</th><th>Gênero</th><th>Artista</th></tr></thead><tbody>')
+                $.each(response, function(i, e) {
+                    tr = $('<tr>').append( $('<td>').text(e.id), $('<td>').text(e.name), $('<td>').text(e.time), $('<td>').text('Folk'), $('<td>').text('Bob Dylan'));
+                    $('#playlistTable').append(tr)
+                });
+            }
+        }
+    })
+});
